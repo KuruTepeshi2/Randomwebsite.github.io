2498dfe8e9874a50637eedb81352e1d2ac9e7370
TuanTuPham <phamtuantukma@gmail.com>
2025-11-02 19:59:28 +0700

    them chuc nang login

---
+++ login.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "register_db";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
+
+    
+    $sql = "SELECT * FROM users WHERE username='$user' AND password='$pass'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        
+        echo "<script>alert('Login successful!'); window.location.href='home.html';</script>";
+    } else {
+        echo "<script>alert('Invalid username or password!'); window.location.href='login.html';</script>";
+    }
+}
+
+$conn->close();
+?>
